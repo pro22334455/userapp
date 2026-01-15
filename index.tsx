adeb72af70c865b42d18051fa8e0ebb8539247bf
pro22334455 <brahim.medjadji.visa@gmail.com>
2026-01-15 03:31:04 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  console.error("Ù„Ù… ÙŠØªÙ… Ø§Ù„Ø¹Ø«ÙˆØ± Ø¹Ù„Ù‰ Ø¹Ù†ØµØ± root ÙÙŠ Ù…Ù„Ù HTML");
+  console.error("لم يتم العثور على عنصر root في ملف HTML");
 } else {
   const root = ReactDOM.createRoot(rootElement);
   root.render(

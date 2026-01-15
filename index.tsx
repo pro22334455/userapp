98a3cda9e4c9a4a97ff35c2d7396449fcbe5f465
pro22334455 <brahim.medjadji.visa@gmail.com>
2026-01-15 03:10:43 +0100

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+  console.error("Ù„Ù… ÙŠØªÙ… Ø§Ù„Ø¹Ø«ÙˆØ± Ø¹Ù„Ù‰ Ø¹Ù†ØµØ± root ÙÙŠ Ù…Ù„Ù HTML");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}

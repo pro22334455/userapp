94274dcc02f6d70d789ce099fea3936f1dfbe290
pro22334455 <brahim.medjadji.visa@gmail.com>
2026-01-15 21:34:01 +0100

    config.ts

--- config.ts
+++ config.ts
@@ -9,6 +9,6 @@ export const APP_TYPE: 'USER' | 'ADMIN' | 'DRIVER' = 'USER';
 // 2. إعدادات قاعدة البيانات (Supabase)
 export const DB_CONFIG = {
   enabled: true, 
-  url: 'https://YOUR_PROJECT_REF.supabase.co', 
-  key: 'YOUR_SUPABASE_ANON_KEY' 
+  url: 'https://zdcngosnxowrycqtmvkt.supabase.co', 
+  key: 'sb_publishable_RhYLt_aihE-tIbliAM_Xcg_FCUOkTEz' 
 };
